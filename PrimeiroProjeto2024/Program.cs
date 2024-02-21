fbfa27b109299da6df9371960040829b8c58f84a
CJ302217X <CJ302217X@LAB01-PC11>
2024-02-21 10:31:37 -0300

    Adicionar arquivos de projeto.

---
+++ PrimeiroProjeto2024/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace PrimeiroProjeto2024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome = "Vinicius";
+            int idade = 17;
+            string cidade = "Campos do Jordão";
+
+            Console.WriteLine("Digite o seu nome:");
+            nome = Console.ReadLine();
+            Console.WriteLine("Digite a sua idade:");
+            idade = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite a sua cidade:");
+            cidade = Console.ReadLine();
+
+
+            Console.WriteLine("Nome: {0}", nome);
+            Console.WriteLine("Idade: {0} anos", idade);
+            Console.WriteLine("Cidade: {0}", cidade);
+            //Console.WriteLine("Nome: {0} \n Idade: {1} anos \n Cidade: {2}", nome, idade, cidade);
+        }
+    }
+}
